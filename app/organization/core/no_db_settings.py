c899d2cc76766d7521e04f343c944aa463065339
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2017-04-04 17:21:42 +0200

    Complete package refactoring to make mezzanine-organization a standalone module

--- app/organization/core/no_db_settings.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-try:
-    from local_settings import *
-except ImportError:
-    pass
-# Test runner with no database creation
-TEST_RUNNER = 'organization.core.tests.NoDbTestRunner'
