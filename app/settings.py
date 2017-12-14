c336f940e194ff6227c4837ceec5d7a38ce2095f
Emilie <zawadzki@ircam.fr>
2017-12-14 15:19:32 +0100

    [settings] : add static path of mezzanine organization

--- app/settings.py
+++ app/settings.py
@@ -179,6 +179,10 @@ STATIC_URL = "/static/"
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 STATIC_ROOT = '/srv/static/'
 
+STATICFILES_DIRS = [
+    '/srv/lib/mezzanine-organization/organization/static'
+]
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
