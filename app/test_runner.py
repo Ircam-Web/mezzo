58fa6adf716f2bc3abb1fd5b84749985ee5134ea
AntoineLbrn <antoinelmh@hotmail.fr>
2018-06-07 10:37:43 +0200

    use manage.py to run tests

---
+++ app/test_runner.py
@@ -0,0 +1,44 @@
+import argparse
+import sys
+import django
+import os
+from django.test.runner import DiscoverRunner
+from django.core import management
+
+class MezzoTestsRunner(DiscoverRunner):
+	
+	
+	def __init__(self, pattern=None, top_level=None, verbosity=1,
+                 interactive=True, failfast=False, keepdb=True,
+                 reverse=False, debug_mode=False, debug_sql=False, parallel=0,
+                 tags=None, exclude_tags=None, **kwargs):
+
+		if kwargs['front']:
+			pattern='tests_front.py'
+		if kwargs['back']:
+			pattern='tests.py'
+		super(MezzoTestsRunner,self).__init__(keepdb=not kwargs['destroydb'],pattern=pattern)
+
+	def run_tests(self, test_labels, extra_tests=None, **kwargs):
+		test_labels+=('organization','/srv/lib/mezzanine-organization/')
+		test_labels+=('agenda','/srv/lib/mezzanine-agenda/')
+		test_labels+=('cartridge','/srv/lib/cartridge/')
+		test_labels+=('mezzanine','/srv/lib/mezzanine/')
+		super(MezzoTestsRunner,self).run_tests(test_labels,extra_tests, **kwargs)
+	
+	@classmethod
+	def add_arguments(cls, parser):
+		parser.add_argument('--front' , help="Only run front tests (selenium)",action="store_true")		
+		parser.add_argument('--back' , help="Only run back tests (faster)",action="store_true")		
+		parser.add_argument("--destroydb",help="Execute test on a new database (may be very long).",action="store_true")
+		super(MezzoTestsRunner,cls).add_arguments(parser)
+		 
+"""def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m' , help="Test a specific module.",metavar="MODULE")
+    parser.add_argument("--front",help="Execute only front tests.",action="store_true")
+    parser.add_argument("--back",help="Execute only back tests.",action="store_true")
+    parser.add_argument("-d","--debug-sql",help="Prints logged SQL queries on failure.",action="store_true")
+    parser.add_argument("--parallel",help="Execute only back tests.",type=int,metavar='[N]')
+    parser.add_argument("-p","--pattern",help="The test matching pattern. Defaults to test*.py.")"""
+
