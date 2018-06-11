01772ba671ebefda2202603fb3f62616b4eeea05
AntoineLbrn <antoinelmh@hotmail.fr>
2018-06-11 11:16:45 +0200

    add parameter to print test labels

--- app/test_runner.py
+++ app/test_runner.py
@@ -7,7 +7,7 @@ from django.core import management
 
 class MezzoTestsRunner(DiscoverRunner):
 	
-	
+	print_labels=False
 	def __init__(self, pattern=None, top_level=None, verbosity=1,
                  interactive=True, failfast=False, keepdb=True,
                  reverse=False, debug_mode=False, debug_sql=False, parallel=0,
@@ -17,19 +17,28 @@ class MezzoTestsRunner(DiscoverRunner):
 			pattern='tests_front.py'
 		if kwargs['back']:
 			pattern='tests.py'
+		self.print_labels=kwargs['printlabels']
 		super(MezzoTestsRunner,self).__init__(keepdb=not kwargs['destroydb'],pattern=pattern)
 
 	def run_tests(self, test_labels, extra_tests=None, **kwargs):
 		if not test_labels:
 			test_labels+=('organization','/srv/lib/mezzanine-organization/')
-			test_labels+=('agenda','/srv/lib/mezzanine-agenda/')
+			test_labels+=('mezzanine_agenda','/srv/lib/mezzanine-agenda/')
 			test_labels+=('cartridge','/srv/lib/cartridge/')
 			test_labels+=('mezzanine','/srv/lib/mezzanine/')
-		super(MezzoTestsRunner,self).run_tests(test_labels,extra_tests, **kwargs)
-	
+		if self.print_labels:
+			self.setup_test_environment()
+			suite = self.build_suite(test_labels, extra_tests)
+			for i in suite:
+				print(i)
+		else:
+			super(MezzoTestsRunner,self).run_tests(test_labels,extra_tests, **kwargs)
+
+
 	@classmethod
 	def add_arguments(cls, parser):
 		parser.add_argument('--front' , help="Only run front tests (selenium)",action="store_true")		
 		parser.add_argument('--back' , help="Only run back tests (faster)",action="store_true")		
 		parser.add_argument("--destroydb",help="Execute test on a new database (may be very long).",action="store_true")
+		parser.add_argument("--printlabels",help="Print all tests executed.",action="store_true")
 		super(MezzoTestsRunner,cls).add_arguments(parser)
